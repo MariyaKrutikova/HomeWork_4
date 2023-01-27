a5cd6971be43edb3eba7c52c49ecd17540f70021
Mariya <appleslover@yandex.ru>
2023-01-27 12:38:57 +0300

    Task_1_new

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,13 +2,13 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
-int GetNumber (string message)
+float GetNumber (string message)
 {
-    int result = 0;
+    float result = 0;
     Console.WriteLine(message);
     while (true)
     {
-        if (int.TryParse(Console.ReadLine(), out result))
+        if (float.TryParse(Console.ReadLine(), out result))
         {
             break;
         }
@@ -20,9 +20,27 @@ int GetNumber (string message)
     return result;
 }
 
-int FindPower (int numberA, int numberB)
+int GetPower (string message)
 {
-    int PowerA = 1;
+    int result = 0;
+    Console.WriteLine(message);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введены некорректные данные. Повторите ввод.");
+        }
+    }
+    return result;
+}
+
+float FindPower(float numberA, int numberB)
+{
+    float PowerA = 1;
     for (int i = 1; i <= numberB; i++)
     {
         PowerA = numberA*PowerA; 
@@ -32,7 +50,7 @@ int FindPower (int numberA, int numberB)
 
 
 
-int numberA = GetNumber("Введите число А: ");
-int numberB = GetNumber("Введите число B: ");
-int resultpower = FindPower(numberA, numberB);
+float numberA = GetNumber("Введите число А: ");
+int numberB = GetPower("Введите число B: ");
+float resultpower = FindPower(numberA, numberB);
 Console.WriteLine($"Число {numberA} в степени {numberB} будет {resultpower}");
